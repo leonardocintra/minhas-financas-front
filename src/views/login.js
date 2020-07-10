c9056065ec6aa7e17bb97f53d7f8047c687bdf0e
Leonardo Cintra <leonardo.cintra@luizalabs.com>
2020-07-10 18:41:20 -0300

    login ok

---
+++ src/views/login.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Login extends React.Component {
+    render() {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-6" style={{
+                        position: 'relative', left: '300px'
+                    }} >
+                        Ola Leonardo
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Login
