ebc3b3a3028e9da8b3f8a942cad3f3a56d6b1eec
Leonardo Cintra <leonardo.cintra@luizalabs.com>
2020-07-10 20:08:55 -0300

    fazendo login

--- src/views/login.js
+++ src/views/login.js
@@ -1,19 +1,46 @@
 import React from 'react';
+import Card from '../components/card';
+import FormGroup from '../components/form-group';
 
 class Login extends React.Component {
-    render() {
-        return (
-            <div className="container">
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6" style={{
+            position: 'relative', left: '300px'
+          }} >
+            <div className="bs-docs-section">
+              <Card title="Login">
                 <div className="row">
-                    <div className="col-md-6" style={{
-                        position: 'relative', left: '300px'
-                    }} >
-                        Ola Leonardo
+                  <div className="col-lg-12">
+                    <div className="bs-component">
+                      <fieldset>
+                        <FormGroup label="Email *" htmlFor="exampleInputEmail">
+                          <input type="email"
+                            className="form-control"
+                            id="exampleInputEmail"
+                            aria-describedby="emailHelp"
+                            placeholder="Digite o Email" />
+                        </FormGroup>
+
+                        <FormGroup label="Senha *" htmlFor="exampleInputPassword">
+                          <input type="email" c
+                            className="form-control"
+                            id="exampleInputPassword"
+                            placeholder="Digite a senha" />
+                        </FormGroup>
+                      </fieldset>
                     </div>
+                  </div>
                 </div>
+              </Card>
             </div>
-        )
-    }
+          </div>
+        </div>
+      </div >
+    )
+  }
 }
 
 export default Login
