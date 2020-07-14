6cdfbb28903a1482200aeaa8a09255775d3af948
Leonardo Cintra <leonardo.cintra@luizalabs.com>
2020-07-14 09:44:55 -0300

    ok cadastro

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import Login from './views/login'
+import CadastroUsuario from './views/cadastroUsuario'
 
 import 'bootswatch/dist/flatly/bootstrap.css'
 import './custom.css'
@@ -10,7 +10,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Login />
+        <CadastroUsuario />
       </div>
     )
   }
